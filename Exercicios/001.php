33d7c9b092da8124476a0eef43f22a4dfa4da283
Juan <107631551+JuanFelipe79@users.noreply.github.com>
2024-08-19 21:48:55 -0300

    Add files via upload

---
+++ Exercicios/001.php
@@ -0,0 +1,24 @@
+<?php
+/*
+Variáveis 
+*/
+
+$variavel_1 = 'texto';
+// Imprimir o valor a variável e o seu tipo
+echo gettype($variavel_1).PHP_EOL;
+
+$variavel_2 = 123;
+// Imprimir o valor a variável e o seu tipo
+echo gettype($variavel_2).PHP_EOL;
+
+$variavel_3 = 1;
+// Imprimir o valor a variável e o seu tipo
+echo gettype($variavel_3).PHP_EOL;
+
+$array = [
+    "foo" => "bar",
+    "bar" => "foo",
+];
+// Imprimir o valor a variável e o seu tipo
+
+?>
